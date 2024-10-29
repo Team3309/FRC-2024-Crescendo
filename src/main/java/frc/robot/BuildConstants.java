7c74d017194370b294a7a82b22c1de76c47c1fdd
chary <smkzachatac@gmail.com>
2024-10-28 20:29:12 -0700

    Updated libraries

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "New-2024 Code";
+  public static final String MAVEN_NAME = "FRC-2024-Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 127;
-  public static final String GIT_SHA = "ccfe41a742cc0c3a135a704b579491f85d478892";
-  public static final String GIT_DATE = "2024-03-19 17:32:54 PDT";
+  public static final int GIT_REVISION = 158;
+  public static final String GIT_SHA = "60ae0a5824abba25e39d33246293eb155f8e6919";
+  public static final String GIT_DATE = "2024-10-28 20:24:39 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-19 20:19:06 PDT";
-  public static final long BUILD_UNIX_TIME = 1710904746937L;
+  public static final String BUILD_DATE = "2024-10-28 20:28:38 PDT";
+  public static final long BUILD_UNIX_TIME = 1730172518519L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
